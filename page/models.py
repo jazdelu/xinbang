6f728abaf1dfb3f9943eb0803eae41b4ffac6ab1
jazdelu <lushizhao@qq.com>
2014-08-04 16:27:45 +0800

    ,

--- page/models.py
+++ page/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Page(models.Model):
 	name = models.CharField(max_length = 128, verbose_name = u'页面名称')
 	banner = models.ImageField(upload_to = 'banner', verbose_name = u'顶部图片', null = True, blank = True)
+	template = models.CharField(max_length = 128, verbose_name = u'选择模版',help_text = u'请选择与页面名称相对应的模版')
 	content = models.TextField(verbose_name = u'页面内容',blank = True, null = True)
 
 	class Meta:
