e1f1b8fdd6ea284a8cde689e46af84e357a9e1b4
jazdelu <lushizhao@qq.com>
2014-08-11 17:50:53 +0800

    .

--- xinbang/views.py
+++ xinbang/views.py
@@ -5,13 +5,17 @@ from post.models import Post
 from util.models import Person, Message
 import datetime
 def homepage(request):
-	zxgg = Post.objects.filter(category__name = u'最新公告')[:4]
-	xwkx = Post.objects.filter(category__name = u'新闻快讯')[:2]
+	zxgg = Post.objects.filter(category__name = u'最新公告')[:3]
+	xwkx = Post.objects.filter(category__name = u'新闻快讯')[:3]
 	today = datetime.datetime.today()
-	persons = Person.objects.filter(birthday__month = today.month).filter(birthday__day = today.day)
+	persons = Person.objects.all()
+	plist = []
+	for p in persons:
+		if p.birthday.isocalendar()[1] == today.isocalendar()[1]:
+			plist.append(p)
 	messages = Message.objects.filter(pub_date__lte = today)[:3]
 	print messages
-	return render_to_response("index.html",{ 'xwkx':xwkx, 'zxgg':zxgg, 'persons':persons, 'messages':messages },context_instance=RequestContext(request))
+	return render_to_response("index.html",{ 'xwkx':xwkx, 'zxgg':zxgg, 'persons':plist, 'messages':messages },context_instance=RequestContext(request))
 
 def list(request):
 	return render_to_response("template_list_text.html",context_instance=RequestContext(request))
