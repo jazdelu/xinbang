332010b16a63b7d790474f3510b9749c118f7d95
jazdelu <lushizhao@qq.com>
2014-08-25 20:40:23 +0800

    .

--- xinbang/views.py
+++ xinbang/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from post.models import Post
 from util.models import Person, Message
+from django.db.models import Q
 import datetime
 def homepage(request):
 	zxgg = Post.objects.filter(category__name = u'最新公告')[:3]
-	xwkx = Post.objects.filter(category__name = u'新闻快讯')[:3]
+	xwkx = Post.objects.filter(
+		Q(category__name = u'新闻快讯')|Q(category__name = u'基层动态')
+		)[:3]
 	today = datetime.datetime.today()
 	persons = Person.objects.all()
 	plist = []
